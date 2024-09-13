cb1d8430a5be16a1ddf958c5d6dd9ad54b5a574e
AleksandraMotor <aleksandramotor@gmail.com>
2024-09-13 20:48:25 +0200

    add ArticleShort

--- src/pages/news/News.tsx
+++ src/pages/news/News.tsx
@@ -1,23 +1,29 @@
-import { Link } from 'react-router-dom';
 import { NewsData } from './NewsData';
-
+import ArticleShort from '../../components/templates/article-short/ArticleShort';
+import paw from '../../assets/icons/paw-white.png';
 import './News.scss';
 
 const News = () => {
 
     const newsList = NewsData.map(article => (
         <li key={article.title}>
-            <Link to={`/news/${article.title}`}>Przejdź do strony artykułu "{article.title}"</Link>
+            <ArticleShort
+                title={article.title} 
+                type={article.type} 
+                img={article.img} 
+                text={article.text}
+            />
         </li>
     ));
 
     return (
-        <div>
-            <h1>Nasze ogłoszenia</h1>
-            <ul>
+        <section className='news'>
+            <img className='news__image' src={paw} alt="paw icon" width={50}/>
+            <h1 className='news__header'>tablica ogłoszeń</h1>
+            <ul className='news__list'>
                 {newsList}
             </ul>
-        </div>
+        </section>
     );
 };
 
